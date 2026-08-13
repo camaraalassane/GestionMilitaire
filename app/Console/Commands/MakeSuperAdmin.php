0066fa0342a14290eec6286715387bc63714700b
ALASSANE <camaraalassane257@gmail.com>
2026-08-13 11:01:00 +0000

    Ajout commande MakeSuperAdmin

---
+++ app/Console/Commands/MakeSuperAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class MakeSuperAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-super-admin {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Donner le rôle super_admin à un utilisateur via son email';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $user = User::where('email', $email)->first();
+
+        if (!$user) {
+            $this->error("Aucun utilisateur trouvé avec l'email : {$email}");
+            return;
+        }
+
+        $user->role = 'super_admin';
+        $user->save();
+
+        $this->info("L'utilisateur {$user->name} ({$email}) est maintenant Super Administrateur.");
+    }
+}

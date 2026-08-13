772e10dde8a7771bec04d5866444e573c308c9a1
ALASSANE <camaraalassane257@gmail.com>
2026-08-13 11:03:33 +0000

    Correction UserSeeder pour role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,11 +17,11 @@ class UserSeeder extends Seeder
         $users = [
             [
                 'id' => 1,
-                'name' => 'Administrateur',
+                'name' => 'Super Administrateur',
                 'email' => 'admin@fama.ml',
                 'email_verified_at' => null,
                 'password' => Hash::make('12345678'),
-                'is_admin' => 1,
+                'role' => 'super_admin',
                 'remember_token' => 'Sqp6yeMhhYG4huHp5uqB0x37TLijmu2wYzgKGOOaeOjjo13qBOz0qBiPszwQ',
                 'created_at' => Carbon::parse('2026-03-07 02:41:21'),
                 'updated_at' => Carbon::parse('2026-03-07 02:41:21')
@@ -32,7 +32,7 @@ class UserSeeder extends Seeder
                 'email' => 'ctt@gmail.com',
                 'email_verified_at' => null,
                 'password' => Hash::make('12345678'),
-                'is_admin' => 0,
+                'role' => 'admin',
                 'remember_token' => null,
                 'created_at' => Carbon::parse('2026-03-17 09:12:35'),
                 'updated_at' => Carbon::parse('2026-03-17 09:12:35')
